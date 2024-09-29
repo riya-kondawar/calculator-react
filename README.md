51af79175922a5b8935d246476d684a21540607a
riiya-kondawar <riya.kondawar@gmail.com>
2024-09-29 13:13:47 +0530

    readme updates

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # calculator-react
  This is a simple calculator bulit using React & Tailwind CSS
+![screenshot-ui](/assets/calc.png)
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// React Calculator 
+// FSD Lab5 activity1
+
 import React from 'react'
 import './App.css'
 import Calculator from './components/Calculator'
